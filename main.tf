3e5fbfec4204217f8125f48ad785437fc0ddb0a4
kurt <kurtainz@googlemail.com>
2025-01-11 11:37:27 +0000

    Updated configuration

--- main.tf
+++ main.tf
@@ -1,6 +1,15 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
-resource "aws_s3_bucket" "terraform-nov-project23" {
-  bucket = "terraform-nov-project23"
+
+resource "aws_s3_bucket" "terraform-nov-project25" {
+  bucket = "terraform-nov-project25"
+}
+
+terraform {
+    backend "s3" {
+      bucket = "terraform-nov-project25"
+      region = "eu-west-2"
+      key = "global/s3/terraform.tfstate"
+    }
   }
